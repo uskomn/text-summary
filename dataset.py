e0129ec037ed26b41d5260b333cb87d8406fbecd
uskomn <cn20031220@outlook.com>
2025-05-17 16:09:30 +0800

    模型完善

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from transformers import BartTokenizer
+from torch.utils.data import DataLoader
 
 def get_dataloaders(config):
     tokenizer = BartTokenizer.from_pretrained(config.model_name)
@@ -11,8 +12,17 @@ def get_dataloaders(config):
         return inputs
 
     dataset = load_dataset("cnn_dailymail", "3.0.0",cache_dir=config.cache_dir)
+    dataset["train"] = dataset["train"].select(range(1000))
     tokenized = dataset.map(preprocess, batched=True, remove_columns=dataset['train'].column_names)
 
     tokenized.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
-    return tokenized
+    train_loader = DataLoader(
+        tokenized["train"],
+        batch_size=config.train_batch_size,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True
+    )
+
+    return train_loader
